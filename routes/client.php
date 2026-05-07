dca6f55196a32ffbfb3568b637c0d87e2feafa8e
Coppens-Anthony <145543157+Coppens-Anthony@users.noreply.github.com>
2026-05-07 14:09:53 +0200

    routes' refactor

--- routes/client.php
+++ routes/client.php
@@ -5,17 +5,16 @@ use App\Http\Controllers\GalleryController;
 use App\Http\Controllers\ServiceController;
 
 Route::view('/', 'pages.client.home')->name('home');
-Route::view('about', 'pages.client.about')->name('about');
-Route::view('/price', 'pages.client.prices')->name('price');
+Route::view('/a-propos', 'pages.client.about')->name('about');
 Route::view('/contact', 'pages.client.contact')->name('contact');
-Route::view('/appointment/date', 'pages.client.appointment.appointment2')->name('appointment2');
-Route::view('/appointment/confirmation', 'pages.client.appointment.appointment3')->name('appointment3');
-Route::view('/legal-notices', 'pages.client.legal_notice')->name('notice');
+Route::view('/rendez-vous/date', 'pages.client.appointment.appointment2')->name('appointment2');
+Route::view('/rendez-vous/confirmation', 'pages.client.appointment.appointment3')->name('appointment3');
+Route::view('/mentions-legales', 'pages.client.legal_notice')->name('notice');
 
-Route::get('/gallery', [GalleryController::class, 'index'])->name('gallery');
+Route::get('/galerie', [GalleryController::class, 'index'])->name('gallery');
 
-Route::get('/prices', [ServiceController::class, 'index'])->name('prices');
-Route::get('/appointment', [AppointmentController::class, 'index'])->name('appointment');
+Route::get('/prestations', [ServiceController::class, 'index'])->name('prices');
+Route::get('/rendez-vous', [AppointmentController::class, 'index'])->name('appointment');
 
 
 Route::view('/login', 'pages.admin.login')->name('login')->middleware('guest');
