179ddf3cdb8ecddbf0bae65809c67db8d31ff1bf
Coppens-Anthony <145543157+Coppens-Anthony@users.noreply.github.com>
2026-05-16 11:56:44 +0200

    period off's confirmation and cancelled appointments in this period

--- resources/views/modals/unavailabilities/⚡create.blade.php
+++ resources/views/modals/unavailabilities/⚡create.blade.php
@@ -7,13 +7,35 @@ use Livewire\Component;
 
 new class extends Component {
     public string $date;
-    public bool $isFullDay = true;
+    public bool $isFullDay = false;
     public ?string $start_at = null;
     public ?string $end_at = null;
+    public bool $contactClient = true;
 
     public function mount(array $params)
     {
         $this->date = $params['date'];
+    }
+
+    #[Computed]
+    public function conflictingAppointments()
+    {
+        if (!$this->isFullDay && ($this->start_at === null || $this->end_at === null)) {
+            return collect();
+        }
+
+        if (!$this->isFullDay && $this->start_at >= $this->end_at) {
+            return collect();
+        }
+
+        $start = $this->date . ' ' . ($this->isFullDay ? '09:00:00' : $this->start_at);
+        $end = $this->date . ' ' . ($this->isFullDay ? '18:00:00' : $this->end_at);
+
+        return Appointment::where(function ($query) use ($start, $end) {
+            $query->whereBetween('start_at', [$start, $end])
+                ->orWhereBetween('end_at', [$start, $end])
+                ->orWhere(fn($q) => $q->where('start_at', '<=', $start)->where('end_at', '>=', $end));
+        })->get();
     }
 
     public function store()
@@ -22,23 +44,19 @@ new class extends Component {
             'isFullDay' => 'boolean',
             'start_at' => 'nullable|required_if:isFullDay,false|date_format:H:i',
             'end_at' => 'nullable|required_if:isFullDay,false|date_format:H:i|after:start_at',
+            'contactClient' => 'boolean:strict'
         ]);
 
-        $start = $this->date . ' ' . ($validated['isFullDay'] ? '09:00:00' : $validated['start_at']);
-        $end = $this->date . ' ' . ($validated['isFullDay'] ? '18:00:00' : $validated['end_at']);
+        if ($this->conflictingAppointments->isNotEmpty()) {
+            foreach ($this->conflictingAppointments as $appointment) {
+                if ($validated['contactClient']) {
+                    // ENVOIE DE MAIL
+                }
+                $appointment->delete();
+            }
 
-        $appointments = Appointment::where(function ($query) use ($start, $end) {
-            $query
-                ->whereBetween('start_at', [$start, $end])
-                ->orWhereBetween('end_at', [$start, $end])
-                ->orWhere(function ($q) use ($start, $end) {
-                    $q->where('start_at', '<=', $start)
-                        ->where('end_at', '>=', $end);
-                });
-        })->get();
-
-        if ($appointments->isNotEmpty()) {
-            $this->dispatch('open_modal', ['modal' => 'modals::unavailabilities.confirm', 'params' => ['count' => $appointments->count()]]);
+            $this->dispatch('action_done', message: 'Période off ajoutée avec succès !');
+            $this->dispatch('close_modal');
         } else {
             Unavailabilities::create([
                 'start_at' => $this->date . ' ' . ($validated['isFullDay'] ? '09:00' : $validated['start_at']),
@@ -58,11 +76,23 @@ new class extends Component {
             </x-global.form.checkbox>
             @if(!$this->isFullDay)
                 <div class="flex flex-col md:flex-row gap-4 my-8">
-                    <x-global.form.input type="time" class="w-full" name="start_at" wire:model="start_at">Heure de début
+                    <x-global.form.input type="time" class="w-full" name="start_at" wire:model.live="start_at">Heure de
+                        début
                     </x-global.form.input>
-                    <x-global.form.input type="time" class="w-full" name="end_at" wire:model="end_at">Heure de fin
+                    <x-global.form.input type="time" class="w-full" name="end_at" wire:model.live="end_at">Heure de fin
                     </x-global.form.input>
                 </div>
+            @endif
+            @if(($this->isFullDay || ($this->start_at != null && $this->end_at != null)) && $this->conflictingAppointments->isNotEmpty())
+                <div class="mt-8">
+                    <p class="mb-8"><span class="text-error">Attention</span>, il y a {{ $this->conflictingAppointments->count() }}
+                        rendez-vous durant cette période. Si vous confirmez, {{ $this->conflictingAppointments->count() > 1 ? 'ils seront annulés' : 'il sera annulé'}}.</p>
+                    <x-global.form.checkbox name="contactClient" wire:model="contactClient">Prévenir les clients par
+                        mail
+                    </x-global.form.checkbox>
+                </div>
+            @else
+
             @endif
             <div class="ml-auto w-fit flex gap-6">
                 <x-global.linkButton.button type="button" title="Fermer la modale" :isSecondary="true"
