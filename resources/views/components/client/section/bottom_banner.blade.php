8b198e311c99472fe7954bdd982527ee713a100f
Coppens-Anthony <145543157+Coppens-Anthony@users.noreply.github.com>
2026-06-13 16:14:03 +0200

    fix background image

--- resources/views/components/client/section/bottom_banner.blade.php
+++ resources/views/components/client/section/bottom_banner.blade.php
@@ -1,10 +1,10 @@
 <section
-    class="bg-[url(../../public/assets/img/originals/hero.jpg)] bg-no-repeat bg-cover p-16 mt-32 max-w-screen mb-16 flex items-center justify-center">
+    class="bg-[url(../../public/assets/img/originals/hero_mobile.jpg)] md:bg-[url(../../public/assets/img/originals/hero.jpg)] bg-no-repeat bg-cover p-16 mt-32 max-w-screen mb-16 flex items-center justify-center">
     <div class="text-center flex flex-col gap-16 items-center">
-        <h2 class="text-[4rem] font-bold text-white">Prêt(e) pour votre prochaine coupe ?</h2>
-        <p class="text-[2rem] text-white">Prenez rendez-vous en quelques clics ou contactez-moi pour toute
+        <h2 class="text-[2rem] md:text-[4rem] font-bold text-white">Prêt(e) pour votre prochaine coupe ?</h2>
+        <p class="text-xl md:text-[2rem] text-white">Prenez rendez-vous en quelques clics ou contactez-moi pour toute
             question.</p>
-        <div class="flex gap-4 mx-auto w-fit">
+        <div class="flex flex-col md:flex-row items-center gap-4 mx-auto w-fit">
             <x-global.link-button.link-button :route="route('appointment')" title="Prendre rendez-vous">
                 Rendez-vous
             </x-global.link-button.link-button>
--- resources/views/components/client/section/top_banner.blade.php
+++ resources/views/components/client/section/top_banner.blade.php
@@ -1,9 +1,9 @@
 <section
-    class="bg-[url(../../public/assets/img/originals/hero.jpg)] bg-no-repeat bg-cover max-w-screen h-[calc(100vh-120px)] mb-32 flex items-center justify-center">
-    <div class="text-center flex flex-col gap-16 items-center" itemtype="https://schema.org/Organization" itemscope>
-        <h2 class="text-[4rem] font-bold text-white" itemprop="legalName">Joana-Coiffure</h2>
-        <p class="text-[2rem] text-white" itemprop="keywords">Coiffeuse & visagiste indépendante à Orp-Jauche</p>
-        <div class="flex gap-4 mx-auto w-fit">
+    class="bg-[url(../../public/assets/img/originals/hero_mobile.jpg)] md:bg-[url(../../public/assets/img/originals/hero.jpg)] bg-no-repeat bg-cover max-w-screen h-[calc(100vh-120px)] mb-32 flex items-center justify-center">
+    <div class="text-center mx-8 md:mx-16 flex flex-col gap-16 items-center" itemtype="https://schema.org/Organization" itemscope>
+        <h2 class="text-[2rem] md:text-[4rem] font-bold text-white" itemprop="legalName">Joana-Coiffure</h2>
+        <p class="text-xl md:text-[2rem] text-white" itemprop="keywords">Coiffeuse & visagiste indépendante à Orp-Jauche</p>
+        <div class="flex flex-col md:flex-row items-center gap-4 mx-auto w-fit">
             <x-global.link-button.link-button :route="route('appointment')" title="Prendre rendez-vous">
                 Rendez-vous
             </x-global.link-button.link-button>
